0231192ae9ace894155fdfc960389ff890b6051b
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2022-11-03 14:02:17 -0400

    fix: no longer hide cursor
    When the cli parser fails, it terminates execution so we cannot show the
    cursor again.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
-use std::io::stdout;
-
-use crossterm::{cursor, ExecutableCommand};
-
 mod cli;
 mod code;
 mod fabric;
 mod git;
 
 fn main() {
-    let mut stdout = stdout();
-    stdout.execute(cursor::Hide).unwrap();
     cli::cli().unwrap();
-    stdout.execute(cursor::Show).unwrap();
 }
