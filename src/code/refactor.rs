a7a24356a2fb6303062e5a21a434f44a047f992f
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2022-11-03 10:21:05 -0400

    refactor: add rename_in_file()

--- src/code/refactor.rs
+++ src/code/refactor.rs
@@ -33,16 +33,25 @@ fn is_text_file(path: &Path) -> bool {
     output.starts_with("text/")
 }
 
+pub fn replace_in_file(path: &Path, from: &str, to: &str) -> Result<(), Error> {
+    if !is_text_file(path) {
+        return Ok(());
+    }
+
+    let mut file = std::fs::read_to_string(path)?;
+    file = file.replace(from, to);
+    std::fs::write(path, file)?;
+    Ok(())
+}
+
 fn recursive_replace(path: &Path, old: &str, new: &str) -> Result<(), Error> {
     for entry in std::fs::read_dir(path)? {
         let entry = entry?;
         let path = entry.path();
         if path.is_dir() {
             recursive_replace(&path, old, new)?;
-        } else if is_text_file(&path) {
-            let content = std::fs::read_to_string(&path)?;
-            let content = content.replace(old, new);
-            std::fs::write(&path, content)?;
+        } else {
+            replace_in_file(&path, old, new)?;
         }
     }
     Ok(())
